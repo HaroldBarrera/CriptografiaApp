b7c183a811e11b7ba93c2186a6d231e99f576a07
Harold Barrera NIño <>
2022-05-20 18:20:53 -0500

    Diseño de criptoparsing
    Se termina el diseño de la pagina para cifrar y descifrar

--- client/src/pages/criptoparsingPage/CriptoparsingPage.js
+++ client/src/pages/criptoparsingPage/CriptoparsingPage.js
@@ -19,13 +19,23 @@ const CriptoparsingPage = () => {
 
   return(
     <>
+    <div className='container bg-dark'>
       <div className="container bg-info">
         <h1>SISTEMA DE CIFRADO-DESCIFRADO</h1>
       </div>
       <div className="container">
-        <input type="file" id="file" onChange={(e) => showFile(e)} value={archivo} />
+        <input type="file" id="file" onChange={(e) => showFile(e)} value={archivo} className='text-light'/>
         <textarea id="contenidomensaje" value={textarea}></textarea>
       </div>
+      <div className='container d-flex justify-content-center'>
+        <button className='btn btn-success'>CIFRAR</button>
+        <button className='btn btn-warning'>DESCIFRAR</button>
+      </div>
+      <div className='container d-flex justify-content-center'>
+        <textarea></textarea>
+        <textarea></textarea>
+      </div>
+    </div>
     </>
   );
 }
