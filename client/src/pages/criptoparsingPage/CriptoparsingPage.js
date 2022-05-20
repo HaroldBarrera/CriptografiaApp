368f958781365a8cb7a824b7edc7c2369767f6eb
Harold Barrera NIño <>
2022-05-20 18:04:37 -0500

    Lector de archivos.txt
    Ahora la pagina puede leer el contenido de un archivo.txt

--- client/src/pages/criptoparsingPage/CriptoparsingPage.js
+++ client/src/pages/criptoparsingPage/CriptoparsingPage.js
@@ -3,18 +3,18 @@ import React, { useState } from 'react';
 const CriptoparsingPage = () => {
 
   const [archivo, setArchivo] = useState(null);//Hook del archivo
+  const [textarea, setTextarea] = useState('');
 
-  function readFile(file) {
-    const reader = new FileReader();
-    reader.onload = function() {
-      editor.value= reader.result; 
-    }
-    reader.readAsText(file);
-  }
-
-  function prueba(event){
-    console.log(event.target.value);
-    readFile(event.target.value);
+  const showFile = async (e) => {
+    e.preventDefault()
+    const reader = new FileReader()
+    reader.onload = async (e) => { 
+      const text = (e.target.result)
+      setTextarea(text);
+      console.log(text)
+      //alert(text)
+    };
+    reader.readAsText(e.target.files[0])
   }
 
   return(
@@ -23,8 +23,8 @@ const CriptoparsingPage = () => {
         <h1>SISTEMA DE CIFRADO-DESCIFRADO</h1>
       </div>
       <div className="container">
-        <input type="file" id="file" onChange={(e) => setArchivo(e.target.files[0])} value={archivo} />
-        <textarea id="contenidomensaje"></textarea>
+        <input type="file" id="file" onChange={(e) => showFile(e)} value={archivo} />
+        <textarea id="contenidomensaje" value={textarea}></textarea>
       </div>
     </>
   );
